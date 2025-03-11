af31cf1b2512351727bd0f9265cfbd6cd3f71c84
unknown <filip.lukas@infis.cz>
2025-03-11 09:23:01 +0100

    text

---
+++ zoo.py
@@ -0,0 +1,19 @@
+#git add*
+#git commit -m "nwm"
+#git push
+
+proměná = input("co je dnes za den? ")
+
+#proměná = typ(proměná)
+
+#if - podmínka
+#elif - podmínka
+#else - podmínka (nesplňuje žádnou zadanou podmínku)
+
+#and - pro spojení (or to nebo to)
+
+print("co chete vypsat", proměná)
+# když chceme vypsat text musí být v ""
+# pro odělení podmínky používáme , nebo +
+
+# Filip Lukáš
